20237a58cc895f2db3f9c3b12ee3d5db0ee2c7e7
Alexander Sanchez <alexander.sanchezm2001@gmail.com>
2024-01-25 11:27:43 -0600

    actualiza encriptador

--- index.js
+++ index.js
@@ -1,82 +1,45 @@
-var ingresarTexto = document.querySelector('.ingresar-texto');
+var textoIngresado = document.querySelector(".ingresar-texto");
 var mensajeFinal = document.querySelector('#texto-resultado');
 var btnEncriptar = document.getElementById("btn-encriptar");
 var btnDesencriptar = document.getElementById('btn-desencriptar');
 
-let mensaje = '';
-let encriptado = '';
-let desencriptado = '';
+var mensaje;
+var txtEncriptado
+var txtDesencriptado
+
+try {
+    //Encriptar las letras
+    btnEncriptar.addEventListener('click', () => {
+        mensaje = textoIngresado.value.toLowerCase();
+    
+        txtEncriptado = mensaje
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, 'imes')
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, 'ober')
+        .replace(/u/gi, 'ufat');
+     
+        mensajeFinal.innerHTML = txtEncriptado;
+    
+    })
+    
+    //Desencriptar las letras
+    btnDesencriptar.addEventListener('click', () => {
+    
+        mensaje = textoIngresado.value.toLowerCase();
+        txtDesencriptado = mensaje
+        .replace(/enter/gi,"e")
+        .replace(/imes/gi,'i')
+        .replace(/ai/gi,"a")
+        .replace(/ober/gi,'o')
+        .replace(/ufat/gi,'u');
+         
+        mensajeFinal.innerHTML = txtDesencriptado;
+    })
+
+} catch (e) {
+    alert(`A ocurrido un error ${e}`)
+}
 
 
-//Encriptar las letras
-btnEncriptar.addEventListener('click', () => {
-    
-    const mensaje = ingresarTexto.value.toLowerCase();
-    
-    function encriptarTexto(encriptado){
-        encriptado = '';
-        for(var i = 0; i < mensaje.length; i++){
-            
-            if(mensaje[i] == 'a'){
-                encriptado += 'ai';
-        
-            }else if(mensaje[i] == 'e'){
-                encriptado += 'enter';
-        
-            }else if(mensaje[i] == 'i'){
-                encriptado += 'imes';
-        
-            }else if(mensaje[i] == 'o'){
-                encriptado += 'ober';
-        
-            }else if(mensaje[i] == 'u'){
-                encriptado += 'ufat';
-            
-            }else{
-                encriptado += mensaje[i];
-            }
-        }
-        return encriptado
-        
-    }
-    const textoEncriptado = encriptarTexto(mensaje);
-    mensajeFinal.innerHTML = textoEncriptado;
-    
-})
-
-
-//Desencriptar las letras 
-btnDesencriptar.addEventListener('click', () => {
-    
-    const mensaje = ingresarTexto.value.toLowerCase();
-    
-    function desencriptarTexto(desencriptado){
-        desencriptado = '';
-        
-        for(var i = 0; i < mensaje.length; i++){
-            
-            if(mensaje[i] == 'ai'){
-                desencriptado += 'a';
-        
-            }else if(mensaje[i] == 'enter'){
-                desencriptado += 'e';
-        
-            }else if(mensaje[i] == 'imes'){
-                desencriptado += 'i';
-        
-            }else if(mensaje[i] == 'ober'){
-                desencriptado += 'o';
-        
-            }else if(mensaje[i] == 'u'){
-                desencriptado += 'ufat';
-            
-            }else{
-                desencriptado += mensaje[i]
-            }
-        }
-        return desencriptado
-        
-    }
-    const textoDesencriptado = desencriptarTexto(mensaje);
-    mensajeFinal.innerHTML = textoDesencriptado;
-})
+  
